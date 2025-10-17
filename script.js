e47a9227be3030066e3f17e1053f6e6304994c66
jordonturiello-dev26 <jordonturiello26@gmail.com>
2025-10-17 18:35:54 +1100

    added hero section

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Type writer hero
+document.addEventListener("DOMContentLoaded", () => {
+  const text = "Hello, I'm Jordon.";
+  const speed = 120;
+  let i = 0;
+  const target = document.getElementById("typewriter-hero");
+
+  target.textContent = "";
+
+  function typeWriter() {
+    if (i < text.length) {
+      target.textContent += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  typeWriter();
+});
+
 // Type writer
 document.addEventListener("DOMContentLoaded", () => {
     const text = "Hey World, I'm Jordon!";
